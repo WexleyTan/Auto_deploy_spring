5977b547be5d6928b279ae2a8c4d582e7a46c1bf
WexleyTan <neathtan1402@gmail.com>
2024-09-17 14:33:20 +0700

    hello world

---
+++ src/main/java/com/example/projectdeploy/ProjectDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectdeploy/ProjectDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
